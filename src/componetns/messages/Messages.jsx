4c3eaf31fe2fdcf66e69554c67454244bbef0bb1
Myakish <myakishevandrey@gmail.com>
2022-03-10 09:24:10 +1000

    add page dialogs

--- src/componetns/messages/Messages.jsx
+++ src/componetns/messages/Messages.jsx
@@ -1,7 +1,27 @@
 import classes from './Messages.module.css';
 
 const Messages = () => {
-  return <div>Сообщения</div>;
+  return (
+    <div className={classes.dialogsWrapper}>
+      <div className={classes.dialogs}>
+        <div className={classes.item}>Андрей</div>
+        <div className={classes.item}>Андрей</div>
+        <div className={classes.item}>Андрей</div>
+        <div className={classes.item}>Андрей</div>
+      </div>
+      <div className={classes.list}>
+        <div className={classes.item}>
+          <div className={classes.message}>Привет</div>
+          <div className={classes.message}>Привет</div>
+          <div className={classes.message}>Как дела</div>
+        </div>
+        <form action='#' className={classes.form}>
+          <input className={classes.input} type='text'></input>
+          <button className={classes.button}>Отправить</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
