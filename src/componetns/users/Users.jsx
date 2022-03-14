f27380a64cc13f3a65d5dfa784f307348b891677
Myakish <myakishevandrey@gmail.com>
2022-03-14 14:37:00 +1000

    refactoring (change function component on class component)

--- src/componetns/users/Users.jsx
+++ src/componetns/users/Users.jsx
@@ -1,42 +1,49 @@
 import classes from './Users.module.css';
 import avatarPhoto from '../../assets/img/avatar.png';
 import * as axios from 'axios';
+import React from 'react';
 
-const Users = props => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(respose => {
-      props.setUsers(respose.data.items);
+      this.props.setUsers(respose.data.items);
     });
   }
 
-  let listUser = props.users.map(u => (
-    <div key={u.id} className={classes.item}>
-      <div className={classes.profile}>
-        <img src={u.photos.small ? u.photos.small : avatarPhoto} alt='avatar' />
-        {u.followed ? (
-          <button
-            onClick={() => {
-              props.toUnFollow(u.id);
-            }}>
-            У вас в друзьях
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              props.toFollow(u.id);
-            }}>
-            Добавить в друзья
-          </button>
-        )}
+  render() {
+    return (
+      <div className={classes.list}>
+        {this.props.users.map((u, i) => (
+          <div key={i} className={classes.item}>
+            <div className={classes.profile}>
+              <img src={u.photos.small ? u.photos.small : avatarPhoto} alt='avatar' />
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    this.props.toUnFollow(u.id);
+                  }}>
+                  У вас в друзьях
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    this.props.toFollow(u.id);
+                  }}>
+                  Добавить в друзья
+                </button>
+              )}
+            </div>
+            <div className={classes.info}>
+              <div className={classes.name}>{u.name}</div>
+              <p>{'u.location.country'}</p>
+              <p>{'u.location.cityName'}</p>
+            </div>
+          </div>
+        ))}
       </div>
-      <div className={classes.info}>
-        <div className={classes.name}>{u.name}</div>
-        <p>{'u.location.country'}</p>
-        <p>{'u.location.cityName'}</p>
-      </div>
-    </div>
-  ));
-  return <div className={classes.list}>{listUser}</div>;
-};
+    );
+  }
+}
 
 export default Users;
