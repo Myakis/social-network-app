a884249017b9293ccb76552e3ec8f5b61af78bed
Myakish <myakishevandrey@gmail.com>
2022-03-14 15:05:20 +1000

    using method componentDidMount

--- src/componetns/users/Users.jsx
+++ src/componetns/users/Users.jsx
@@ -6,43 +6,41 @@ import React from 'react';
 class Users extends React.Component {
   constructor(props) {
     super(props);
+  }
+  componentDidMount() {
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(respose => {
       this.props.setUsers(respose.data.items);
     });
   }
-
   render() {
-    return (
-      <div className={classes.list}>
-        {this.props.users.map((u, i) => (
-          <div key={i} className={classes.item}>
-            <div className={classes.profile}>
-              <img src={u.photos.small ? u.photos.small : avatarPhoto} alt='avatar' />
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    this.props.toUnFollow(u.id);
-                  }}>
-                  У вас в друзьях
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    this.props.toFollow(u.id);
-                  }}>
-                  Добавить в друзья
-                </button>
-              )}
-            </div>
-            <div className={classes.info}>
-              <div className={classes.name}>{u.name}</div>
-              <p>{'u.location.country'}</p>
-              <p>{'u.location.cityName'}</p>
-            </div>
-          </div>
-        ))}
+    let listUser = this.props.users.map((u, i) => (
+      <div key={i} className={classes.item}>
+        <div className={classes.profile}>
+          <img src={u.photos.small ? u.photos.small : avatarPhoto} alt='avatar' />
+          {u.followed ? (
+            <button
+              onClick={() => {
+                this.props.toUnFollow(u.id);
+              }}>
+              У вас в друзьях
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                this.props.toFollow(u.id);
+              }}>
+              Добавить в друзья
+            </button>
+          )}
+        </div>
+        <div className={classes.info}>
+          <div className={classes.name}>{u.name}</div>
+          <p>{'u.location.country'}</p>
+          <p>{'u.location.cityName'}</p>
+        </div>
       </div>
-    );
+    ));
+    return <div className={classes.list}>{listUser}</div>;
   }
 }
 
